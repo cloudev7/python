cf1f42fca2570b939de5e14a736565711db61928
rumiismail@gmail.com <rumiismail@gmail.com>
2017-05-08 15:10:35 +0530

    lamda add more expressions

--- lambda/lambda.py
+++ lambda/lambda.py
@@ -1,5 +1,9 @@
 #!/usr/local/bin/perl
 
 add = lambda x, y : x + y
+subtract = lambda x, y : x - y
+multiply = lambda x, y : x * y
 
 print( add(10,20) )
+print( subtract(10,20) )
+print( multiply(10,20) )
